7840e9a8df4598962e262b5620afab847a06ae4e
mluedke2 <mluedke2@gmail.com>
2015-10-08 20:40:08 -0700

    add music code

---
+++ Karenina/MusicStep.swift
@@ -0,0 +1,13 @@
+//
+//  MusicStep.swift
+//  Karenina
+//
+//  Created by Matt Luedke on 10/8/15.
+//  Copyright © 2015 Razeware. All rights reserved.
+//
+
+import ResearchKit
+
+class MusicStep: ORKActiveStep {
+  var clip: MusicClip!
+}
---
+++ Karenina/MusicStepViewController.swift
@@ -0,0 +1,32 @@
+//
+//  MusicStepViewController.swift
+//  Karenina
+//
+//  Created by Matt Luedke on 10/8/15.
+//  Copyright © 2015 Razeware. All rights reserved.
+//
+
+import AVFoundation
+import ResearchKit
+
+class MusicStepViewController: ORKActiveStepViewController {
+  
+  var audioPlayer: AVAudioPlayer?
+  
+  override func start() {
+    super.start()
+    
+    if let step = step as? MusicStep {
+      do {
+        try audioPlayer = AVAudioPlayer(contentsOfURL: step.clip.fileURL(),
+          fileTypeHint: AVFileTypeMPEGLayer3)
+        audioPlayer?.play()
+      } catch {}
+    }
+  }
+  
+  override func stepDidFinish() {
+    super.stepDidFinish()
+    audioPlayer?.stop()
+  }
+}
